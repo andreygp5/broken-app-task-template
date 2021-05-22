59437fd7c5aa361ca1d78e0f6ef6e77370619b50
andreygp5 <andreygp555@gmail.com>
2021-05-22 18:18:09 +0300

    refactor: use prettier

--- app.js
+++ app.js
@@ -1,17 +1,16 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const db = require('./db');
-const user = require('./controllers/usercontroller');
-const game = require('./controllers/gamecontroller')
+const db = require("./db");
+const user = require("./controllers/usercontroller");
+const game = require("./controllers/gamecontroller");
 
-require('dotenv').config()
-
+require("dotenv").config();
 
 db.sync();
 app.use(express.json());
-app.use('/api/auth', user);
-app.use(require('./middleware/validate-session'))
-app.use('/api/game', game);
+app.use("/api/auth", user);
+app.use(require("./middleware/validate-session"));
+app.use("/api/game", game);
 app.listen(process.env.APP_PORT, function () {
     console.log(`App is listening on ${process.env.APP_PORT}`);
-})
+});
