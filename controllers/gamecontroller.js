b1c36093d91548bbc4e743343882a8c8763b7755
andreygp5 <andreygp555@gmail.com>
2021-05-22 17:25:05 +0300

    fix: game may be created with non-existent user owner

--- controllers/gamecontroller.js
+++ controllers/gamecontroller.js
@@ -1,10 +1,13 @@
+const user = require('../models/user');
+
 var router = require('express').Router();
 var Game = require('../db').import('../models/game');
+var User = require('../db').import('../models/user');
 
 router.get('/all', (req, res) => {
     Game.findAll({ where: { owner_id: req.user.id } })
         .then(
-            function findSuccess(data) {
+            function findSuccess(data) { //todo: replace games with data
                 res.status(200).json({
                     games: games,
                     message: "Data fetched."
@@ -37,26 +40,41 @@ router.get('/:id', (req, res) => {
 })
 
 router.post('/create', (req, res) => {
-    Game.create({
-        title: req.body.game.title,
-        owner_id: req.body.user.id,
-        studio: req.body.game.studio,
-        esrb_rating: req.body.game.esrb_rating,
-        user_rating: req.body.game.user_rating,
-        have_played: req.body.game.have_played
-    })
-        .then(
-            function createSuccess(game) {
-                res.status(200).json({
-                    game: game,
-                    message: "Game created."
+    const userNotFoundHandler = () => res.status(404).json({
+        message: "User provided like owner not found"
+    });
+    User.findOne({ where: { id: req.body.user.id } })
+        .then((user) => {
+            if (user) {
+                Game.create({
+                    title: req.body.game.title,
+                    owner_id: req.body.user.id,
+                    studio: req.body.game.studio,
+                    esrb_rating: req.body.game.esrb_rating,
+                    user_rating: req.body.game.user_rating,
+                    have_played: req.body.game.have_played
                 })
-            },
+                    .then(
+                        function createSuccess(game) {
+                            res.status(200).json({
+                                game: game,
+                                message: "Game created."
+                            })
+                        },
 
-            function createFail(err) {
-                res.status(500).send(err.message)
+                        function createFail(err) {
+                            res.status(500).send(err.message)
+                        }
+                    )
+            } else {
+                userNotFoundHandler();
             }
-        )
+        })
+        .catch((err) => {
+            res.status(500).json({
+                message: err.message
+            })
+        });
 })
 
 router.put('/update/:id', (req, res) => {
@@ -70,7 +88,7 @@ router.put('/update/:id', (req, res) => {
         {
             where: {
                 id: req.params.id,
-                owner_id: req.user
+                owner_id: req.user //todo: user.id
             }
         })
         .then(
